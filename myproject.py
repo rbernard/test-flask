ed9bad91b077733ece48abd08487cc64fdaab6c4
Rémy BERNARD <remy@Remys-MacBook-Pro.local>
2020-10-02 23:12:07 -0700

    mysql connection

--- myproject.py
+++ myproject.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, url_for, render_template, request
 from flask_mysqldb import MySQL
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -15,14 +15,29 @@ mysql = MySQL(app)
 def index():
     if request.method == "POST":
         details = request.form
-        firstName = details['fname']
-        lastName = details['lname']
+        first_name = details['fname']
+        last_name = details['lname']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        cur.execute("INSERT INTO Users(firstName, lastName) VALUES (%s, %s)", (first_name, last_name))
         mysql.connection.commit()
         cur.close()
-        return 'success'
-    return render_template('index.html')
+        return display_page()
+    return display_page()
+
+@app.route('/delete_user/<user_id>', methods=['POST'])
+def delete_user(user_id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM Users WHERE id = %s", [user_id])
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('index'))
+
+
+def display_page():
+    cursor = mysql.connection.cursor()
+    cursor.execute("select firstName, lastName, id from Users")
+    data = cursor.fetchall()
+    return render_template('index.html', value=data)
 
 
 if __name__ == '__main__':
---
+++ tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Users (
+    id int NOT NULL AUTO_INCREMENT,
+    FirstName varchar(255),
+    LastName varchar(255),
+    PRIMARY KEY (id)
+);
