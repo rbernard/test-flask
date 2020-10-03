f243c564f36b1613487d69708b84de91e5d627d3
Rémy BERNARD <remy@Remys-MacBook-Pro.local>
2020-10-03 15:46:20 -0700

    select letters

--- myproject.py
+++ myproject.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request
 from flask_mysqldb import MySQL
+import sys
 
 app = Flask(__name__)
 
@@ -14,15 +15,16 @@ mysql = MySQL(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
-        details = request.form
-        first_name = details['fname']
-        last_name = details['lname']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO Users(firstName, lastName) VALUES (%s, %s)", (first_name, last_name))
-        mysql.connection.commit()
-        cur.close()
+        # first_name = request.form['fname']
+        # last_name = request.form['lname']
+        # cur = mysql.connection.cursor()
+        # cur.execute("INSERT INTO Users(firstName, lastName) VALUES (%s, %s)", (first_name, last_name))
+        # mysql.connection.commit()
+        # cur.close()
         return display_page()
+
     return display_page()
+
 
 @app.route('/delete_user/<user_id>', methods=['POST'])
 def delete_user(user_id):
@@ -34,10 +36,44 @@ def delete_user(user_id):
 
 
 def display_page():
+    first_letters = read_first_letters()
+    first_letter_selected = request.args.get('firstLetterSelected')
+    second_letters = read_second_letters(first_letter_selected)
+    second_letter_selected = request.args.get('secondLetterSelected')
+    words = read_words(first_letter_selected, second_letter_selected)
+
+    print(request.args)
+    return render_template('index.html', first_letters=first_letters, second_letters=second_letters, words=words)
+
+
+def read_first_letters():
     cursor = mysql.connection.cursor()
-    cursor.execute("select firstName, lastName, id from Users")
-    data = cursor.fetchall()
-    return render_template('index.html', value=data)
+    cursor.execute("select DISTINCT LEFT(Word, 1) FROM WORDS")
+    return cursor.fetchall()
+
+
+def read_second_letters(first_letter):
+    if first_letter is None:
+        return []
+    cursor = mysql.connection.cursor()
+    cursor.execute("select DISTINCT LEFT(Word, 2) FROM WORDS WHERE LEFT(Word, 1) = %s", first_letter)
+    return cursor.fetchall()
+
+
+def read_words(first_letter, second_letter):
+    print(first_letter)
+    print(second_letter)
+    if first_letter is None:
+        return []
+
+    if second_letter is None:
+        cursor = mysql.connection.cursor()
+        cursor.execute("select Word FROM WORDS WHERE LEFT(Word, 1) = %s", [first_letter])
+        return cursor.fetchall()
+
+    cursor = mysql.connection.cursor()
+    cursor.execute("select Word FROM WORDS WHERE LEFT(Word, 2) = %s", [second_letter])
+    return cursor.fetchall()
 
 
 if __name__ == '__main__':
--- tables.sql
+++ tables.sql
@@ -4,3 +4,10 @@ CREATE TABLE Users (
     LastName varchar(255),
     PRIMARY KEY (id)
 );
+
+CREATE TABLE Words (
+    id int NOT NULL AUTO_INCREMENT,
+    Word varchar(255),
+    PRIMARY KEY (id)
+);
+
